cef79505f03fe9c4f0c9fa6d556ddc10650bf63b
ErikGarfia <erik-22-34-21@hotmail.com>
2020-07-14 16:39:37 -0500

    Updates

---
+++ precache-manifest.fda47af15459ebe88c0e2b7a663803c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfbf73acefda9b98b224776845b0e9f",
+    "url": "/ErikGarfiaportfolio/index.html"
+  },
+  {
+    "revision": "fc4d1aa1117f72c811c6",
+    "url": "/ErikGarfiaportfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "88080c94049add16e45c",
+    "url": "/ErikGarfiaportfolio/static/css/main.9eb532eb.chunk.css"
+  },
+  {
+    "revision": "fc4d1aa1117f72c811c6",
+    "url": "/ErikGarfiaportfolio/static/js/2.4eabe596.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/ErikGarfiaportfolio/static/js/2.4eabe596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88080c94049add16e45c",
+    "url": "/ErikGarfiaportfolio/static/js/main.be3a34a6.chunk.js"
+  },
+  {
+    "revision": "be6e3c865f5f06d0423f",
+    "url": "/ErikGarfiaportfolio/static/js/runtime-main.754109d2.js"
+  },
+  {
+    "revision": "61bfe9f9a022ccb4982d9dc8a1c0be6e",
+    "url": "/ErikGarfiaportfolio/static/media/resume.61bfe9f9.pdf"
+  }
+]);
