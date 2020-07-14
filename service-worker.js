6af6de7f5233f1ab4e280107709ca51fdb10681e
ErikGarfia <erik-22-34-21@hotmail.com>
2020-07-14 17:12:25 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ErikGarfiaportfolio/precache-manifest.fda47af15459ebe88c0e2b7a663803c9.js"
+  "/ErikGarfiaportfolio/precache-manifest.3689ba11049c4427b3eb1b68985a298d.js"
 );
 
 self.addEventListener('message', (event) => {
